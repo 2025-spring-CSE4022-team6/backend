c6dd9968f4d7df2f0c4886dcf00fd837ef30d2fa
naaadang <126650849+naaadang@users.noreply.github.com>
2025-04-09 19:22:39 +0900

    Update SimpleReviewDto.java

--- src/main/java/swteam6/backend/dto/response/SimpleReviewDto.java
+++ src/main/java/swteam6/backend/dto/response/SimpleReviewDto.java
@@ -6,9 +6,7 @@ import lombok.Setter;
 @Getter @Setter
 public class SimpleReviewDto {
     private String title;
-<<<<<<< HEAD
-    private double Score;
-=======
+
     private double score;
->>>>>>> afb9f62e89a039abc2c3660b31fb5bd357391fbb
+
 }
