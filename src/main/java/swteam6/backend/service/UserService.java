a87571ced8dd409f4277d8239024e6deaccbffca
kyuhyunglee <102961109+kyuhyunglee@users.noreply.github.com>
2025-05-14 20:44:25 +0900

    Update UserService.java
    "회원가입과 로그인 시에 예외 상황 로직 추가, 출력 메세지 수정"

--- src/main/java/swteam6/backend/service/UserService.java
+++ src/main/java/swteam6/backend/service/UserService.java
@@ -24,8 +24,12 @@ public class UserService {
     //회원가입
     @Transactional
     public UserResponseDto signup(UserSignupDto signupDto) {
+        
+        if (userRepository.existsByEmail(signupDto.getEmail())) {
+            throw new IllegalArgumentException("이미 존재하는 이메일입니다.");
+        }
+        
         User user = signupDto.toEntity(passwordEncoder);
-
         User savedUser = userRepository.save(user);
         return UserResponseDto.fromEntity(savedUser);
     }
@@ -33,7 +37,7 @@ public class UserService {
     //로그인
     public LoginResponseDto login(LoginRequestDto req) {
         User user = userRepository.findByEmail(req.getEmail())
-                .orElseThrow(() -> new BadCredentialsException("잘못된 이메일 또는 비밀번호입니다."));
+                .orElseThrow(() -> new BadCredentialsException("존재하지 않는 유저입니다."));
 
         if (!passwordEncoder.matches(req.getPassword(), user.getPassword())) {
             throw new BadCredentialsException("잘못된 이메일 또는 비밀번호입니다.");
