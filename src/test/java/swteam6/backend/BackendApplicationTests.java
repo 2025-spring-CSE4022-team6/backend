f132251303d8890bbf64eaf0c09eaec427be0209
KyuHyung Lee <kyuhunglee01@gmail.com>
2025-05-15 16:42:29 +0900

    Initial CI/CD test settings

--- src/test/java/swteam6/backend/BackendApplicationTests.java
+++ src/test/java/swteam6/backend/BackendApplicationTests.java
@@ -1,18 +1,14 @@
 package swteam6.backend;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
 import swteam6.backend.service.ReviewService;
-import swteam6.backend.dto.request.UserSignupDto;
-import swteam6.backend.dto.response.UserResponseDto;
 import swteam6.backend.service.UserService;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BackendApplicationTests {
 
 	@Autowired
@@ -20,25 +16,6 @@ class BackendApplicationTests {
 
 	@Autowired
 	UserService userService;
-
-	@Test
-	@Transactional
-	@DisplayName("UserService - 회원가입 성공 테스트")
-	void signup_success() throws Exception {
-		// given
-		UserSignupDto signupDto = new UserSignupDto();
-		signupDto.setEmail("junittest@example.com");
-		signupDto.setNickname("JUnit유저");
-		signupDto.setProfilePath("/images/test.png");
-
-		// when
-		UserResponseDto result = userService.signup(signupDto);
-
-		// then
-		assertNotNull(result.getId());
-		assertEquals("junittest@example.com", result.getEmail());
-		assertEquals("JUnit유저", result.getNickname());
-	}
 
 	@Test
 	void contextLoads() {
