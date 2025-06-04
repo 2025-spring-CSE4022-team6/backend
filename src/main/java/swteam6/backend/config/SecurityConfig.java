94508be52b29f85b32c20bd52a2a209c9c333fe4
naaadang <126650849+naaadang@users.noreply.github.com>
2025-06-04 15:30:25 +0900

    Update SecurityConfig.java

--- src/main/java/swteam6/backend/config/SecurityConfig.java
+++ src/main/java/swteam6/backend/config/SecurityConfig.java
@@ -66,6 +66,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.addAllowedOrigin("http://localhost:5173"); // 허용
+        configuration.addAllowedOrigin("http://localhost:3000");
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS")); // 허용 메서드
         configuration.addAllowedHeader("*");     // 모든 헤더 허용
         configuration.setAllowCredentials(true); // 쿠키/Authorization 허용
