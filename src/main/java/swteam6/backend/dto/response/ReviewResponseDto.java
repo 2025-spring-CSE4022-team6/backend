bc3c46aadecdb497615204373d78d3ad297e47a2
KyuHyung Lee <kyuhunglee01@gmail.com>
2025-06-16 17:46:10 +0900

    Fix: added place features to Review writing and searching APIS

--- src/main/java/swteam6/backend/dto/response/ReviewResponseDto.java
+++ src/main/java/swteam6/backend/dto/response/ReviewResponseDto.java
@@ -17,19 +17,34 @@ public class ReviewResponseDto {
     private final String comment;
     private final double score;
     private final List<Tag> tags;
+    private String location;
+    private String cuisine;
+    private String imagePath;
+    private double latitude;
+    private double longitude;
+    private int totalReviews;
 
     @Builder
-    public ReviewResponseDto(Long id, Long userId, Long placeId, String title, String comment, double score, List<Tag> tags) {
+    public ReviewResponseDto(Long id, Long userId, Long placeId, String title, String comment, double score, String location, String cuisine, String imagePath,
+                             double latitude, double longitude, int totalReviews, List<Tag> tags) {
         this.id = id;
         this.userId = userId;
         this.placeId = placeId;
         this.title = title;
         this.comment = comment;
         this.score = score;
+        this.location = location;
+        this.cuisine = cuisine;
+        this.imagePath = imagePath;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.totalReviews = totalReviews;
         this.tags = tags;
     }
 
     public static ReviewResponseDto fromEntity(Review review) {
+        var place = review.getPlace();
+
         return ReviewResponseDto.builder()
                 .id(review.getId())
                 .userId(review.getUser().getId())
@@ -37,6 +52,12 @@ public class ReviewResponseDto {
                 .title(review.getTitle())
                 .comment(review.getComment())
                 .score(review.getScore())
+                .location(place.getLocation())
+                .cuisine(place.getCuisine().name())
+                .imagePath(place.getImagePath())
+                .latitude(place.getLatitude())
+                .longitude(place.getLongitude())
+                .totalReviews(place.getTotalReviews())
                 .tags(review.getTags())
                 .build();
     }
